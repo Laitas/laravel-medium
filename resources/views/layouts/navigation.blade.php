8596645f765cac541ef2d28ab0560f7bc0aa8501
Laitas <61273117+Laitas@users.noreply.github.com>
2025-08-19 18:58:15 +0300

    Update profile

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -31,7 +31,7 @@
                         <x-slot name="trigger">
                             <button
                                 class="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none">
-                                <div>{{ Auth::user()->name }}</div>
+                                <div>{{ Auth::user()->username }}</div>
                                 <div class="ms-1">
                                     <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
                                         viewBox="0 0 20 20">
@@ -89,7 +89,8 @@
         <!-- Responsive Settings Options -->
         <div class="border-t border-gray-200 pb-1 pt-4">
             <div class="px-4">
-                <div class="text-base font-medium text-gray-800">{{ Auth::user()->name }}</div>
+                <div class="text-base font-medium text-gray-800">{{ Auth::user()->username }}</div>
+                <div class="text-sm font-medium text-gray-500">{{ Auth::user()->name }}</div>
                 <div class="text-sm font-medium text-gray-500">{{ Auth::user()->email }}</div>
             </div>
 
